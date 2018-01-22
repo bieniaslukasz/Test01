2eff20e593ac48ec974b66d12e27cb9880aba82c
bieniaslukasz <s162377@student.dtu.dk>
2018-01-22 16:27:15 +0100

    First commit

---
+++ Test01/src/Test01.java
@@ -0,0 +1,13 @@
+
+public class Test01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test01 onboard");
+
+	}
+
+}
